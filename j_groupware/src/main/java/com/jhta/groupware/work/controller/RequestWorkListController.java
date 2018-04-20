2ae58c00a661b8b68c02876253bfdb3492acc0e3
kms228 <ylloviznar@gmail.com>
2018-04-20 12:22:52 +0900

    work : controller update

--- j_groupware/src/main/java/com/jhta/groupware/work/controller/RequestWorkListController.java
+++ j_groupware/src/main/java/com/jhta/groupware/work/controller/RequestWorkListController.java
@@ -1,5 +1,12 @@
 package com.jhta.groupware.work.controller;
 
-public class RequestWorkListController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class RequestWorkListController {
+	@RequestMapping("/requestWorkList")
+	public String work_requestList() {
+		return ".work.work_requestList";
+	}
 }
--- j_groupware/src/main/java/com/jhta/groupware/work/controller/WorkController.java
+++ j_groupware/src/main/java/com/jhta/groupware/work/controller/WorkController.java
@@ -4,23 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WorkController {
-
-	@RequestMapping("/requestWorkList")
-	public String work_requestList() {
-		return ".work.work_requestList";
-	}
-	@RequestMapping("/responseWorkList")
-	public String work_responseList() {
-		return ".work.work_responseList";
-	}
 	@RequestMapping("/work")
 	public String work() {
 		return ".work.work";
-	}
-	
-	@RequestMapping("/workList")
-	public String work_list() {
-		return ".work.work_list";
 	}
 	
 }
