2bce5d844cfb17e32a86e0f801ccc25a852cdb72
zazuuka <zazuuka@gmail.com>
2018-04-25 10:59:05 +0900

    일정 수정,삭제 구현

--- j_groupware/src/main/java/com/jhta/groupware/calendar/service/CalendarService.java
+++ j_groupware/src/main/java/com/jhta/groupware/calendar/service/CalendarService.java
@@ -1,6 +1,5 @@
 package com.jhta.groupware.calendar.service;
 
-import java.text.SimpleDateFormat;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -25,27 +24,48 @@ public class CalendarService {
 			System.out.println("파일의 이름은?:"+file1.getName());			
 			ManipulateFile file = new ManipulateFile(file1,session);		
 			String saveFileName = file.copyFileToServer("/resources/upload/calendar");
-			System.out.println(saveFileName);
+//			System.out.println(saveFileName);
 			Sc_FileVo vo1 = new Sc_FileVo(0, saveFileName, file1.getOriginalFilename(), file1.getSize(), dao.getSch_num(vo.getEmp_num()));
-			result = dao.sc_File(vo1);				
+			result = dao.insertSc_File(vo1);				
 		}				
 		return result;
 	}
+	public Sc_FileVo getSc_file(int sch_num) {
+		return dao.getSc_file(sch_num);
+	}
 	public int update(ScheduleVo vo, MultipartFile file1, HttpSession session) {
-		return 0;
+		int result = 0;
+		result = dao.updateSchedule(vo);
+		if(file1.isEmpty()==false) {
+			Sc_FileVo preFile=getSc_file(vo.getSch_num());
+			if(preFile!=null) {
+				System.out.println("수정하려고 하는 튜플에 파일이 있음을 확인");				
+				result = deleteFile(session, preFile);
+				result = deleteSc_File(vo.getSch_num());
+			}
+			
+			ManipulateFile file = new ManipulateFile(file1,session);
+			String saveFileName = file.copyFileToServer("/resources/upload/calendar");
+//			System.out.println(saveFileName);
+			Sc_FileVo vo1 = new Sc_FileVo(0, saveFileName, file1.getOriginalFilename(), file1.getSize(), dao.getSch_num(vo.getEmp_num()));
+			result = dao.insertSc_File(vo1);
+			
+			//System.out.println("CalendarService Sch_num : "+vo.getSch_num());
+			
+		}
+		return result;
 	}
 	
 	public List<Sc_FileVo> getSchedules(String id) {
 		List<Sc_FileVo> vo = dao.getSchedules(id);
-		for(Sc_FileVo vo1:vo) {
-			SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");			
-			
-			
-			System.out.println("시작일:"+vo1.getSch_sdate().getTime());
-			System.out.println("시작일:"+sdf1.format(vo1.getSch_sdate()));
-			System.out.println("종료일:"+vo1.getSch_edate().getTime());
-			System.out.println("종료일:"+sdf1.format(vo1.getSch_edate()));
-		}
+//		for(Sc_FileVo vo1:vo) {
+//			SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");			
+//						
+//			System.out.println("시작일:"+vo1.getSch_sdate().getTime());
+//			System.out.println("시작일:"+sdf1.format(vo1.getSch_sdate()));
+//			System.out.println("종료일:"+vo1.getSch_edate().getTime());
+//			System.out.println("종료일:"+sdf1.format(vo1.getSch_edate()));
+//		}
 		return vo; 
 	}
 
@@ -56,4 +76,21 @@ public class CalendarService {
 	public List<PositionVo> getPosition() {
 		return dao.getPosition();
 	}	
+	private int deleteFile(HttpSession session, Sc_FileVo vo) {
+		int result = 0;
+		String fileName = session.getServletContext().getRealPath(vo.getSf_savefilename());
+		result = new ManipulateFile().deleteFile(fileName);
+		return result;
+	}
+	private int deleteSc_File(int sch_num) {
+		return dao.deleteSc_File(sch_num);
+	}
+	public int deleteSchedule(Sc_FileVo vo,HttpSession session) {
+		String savefile=vo.getSf_savefilename();
+		if(!(savefile==null)) {
+			deleteFile(session,vo);
+			deleteSc_File(vo.getSch_num());
+		}
+		return dao.deleteSchedule(vo.getSch_num());
+	}
 }
